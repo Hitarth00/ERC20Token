8daf8b8f1b17f63943f7a7731e2dbad1e46278c3
19IT117 <19it117@charusat.edu.in>
2022-07-21 18:29:01 +0530

    crowd sale contract

--- contracts/CrowdSale.sol
+++ contracts/CrowdSale.sol
@@ -5,10 +5,21 @@ contract CrowdSale{
   address public admin;
   IERC20Interface public tokenContract;
   uint256 public tokenPrice;
+  uint256 public tokensSold;
+  event Sell(address _buyer, uint256 _amount);
   constructor(IERC20Interface _tokenContract,uint256 _price){
     admin = msg.sender;
     tokenContract = _tokenContract;
     tokenPrice = _price;
   }
-
+  function buyTokens(uint256 _numberOfTokens) public payable{
+      //Require msg.value >= numberOfTokens * tokenPrice
+      require(msg.value == _numberOfTokens * tokenPrice);
+      require(tokenContract.balanceOf(address(this))>= _numberOfTokens);
+      // transfer successfull
+      //keep track of number of tokens
+      tokensSold += _numberOfTokens;
+      // Emit Event
+      emit Sell(msg.sender, _numberOfTokens);
+  }
 }
--- test/CrowdSale.js
+++ test/CrowdSale.js
@@ -1,7 +1,12 @@
+const ERC20 = artifacts.require("Cryptos");
 const CrowdSale = artifacts.require("CrowdSale");
 contract('CrowdSale',function(accounts){
   var tokenSaleInstance;
+  var tokenInstance;
+  var buyer = '0x3591C2C22C7E831d49feC94Bd407ED63aa35fCF0';
+  var admin = '0xA1F0242e2F1ea7b940B5BDaf00AC1c4Cc9b753D0';
   var tokenPrice = 1000;
+  var tokenAvailable = 750000;
   it('initialize the contract with correct value', function(){
     return CrowdSale.deployed().then(function(instance){
       tokenSaleInstance = instance;
@@ -17,8 +22,35 @@ contract('CrowdSale',function(accounts){
       return tokenSaleInstance.tokenPrice();
     }).then(function(price){
       assert.equal(tokenPrice,price,"tokenPrice is equal to price")
+    })  
+  });
+  it('buying tokens', function(){
+      numberOfTokens = 10;
+      value = numberOfTokens*tokenPrice;
+      
+    return ERC20.deployed().then(function(instance){
+      tokenInstance = instance;
+      return CrowdSale.deployed()
+    }).then(function(instance){
+       tokenSaleInstance = instance; 
+       return tokenInstance.transfer(tokenSaleInstance.address,tokenAvailable,{from : admin});
+    }).then(function(success){
+      return tokenSaleInstance.buyTokens(numberOfTokens, {from : buyer,value : value});
+    }).then(function(receipt){
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell" event');
+      assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchased the tokens');
+      assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of tokens purchased');
+      return tokenSaleInstance.tokensSold();
+    }).then(function(amount){
+      assert.equal(amount.toNumber(),numberOfTokens,"10 tokens sold till now");
+      numberOfTokens = 800000;
+      value = numberOfTokens*tokenPrice;
+      return tokenSaleInstance.buyTokens(numberOfTokens, {from : buyer,value : value});
+    }).then(assert.fail).catch(function(receipt){
+      var soldToken = tokenSaleInstance.tokensSold()
+      assert.equal(soldToken,numberOfTokens,"10 tokens sold till now");
     })
-    
   });
  
 });
