52d601bd88afd1e25d411c754b05c37a6bea5ae0
19IT117 <19it117@charusat.edu.in>
2022-07-21 10:29:57 +0530

    updated test case for allowance function

--- test/ERC20.js
+++ test/ERC20.js
@@ -44,4 +44,22 @@ contract('Cryptos',function(accounts){
       assert.equal(balance.toNumber(),10, "check balance after transfer");
     })
   });
+  it('Delegated transfer using approve', function(){
+    return ERC20.deployed().then(function(instance){
+      tokenInstance = instance; 
+      return tokenInstance.approve.call(accounts[1],100);
+    }).then(function(success){
+      assert.equal(success.toString(),"true","It returns true");
+      return tokenInstance.approve(accounts[1],100,{from : accounts[0]});
+    }).then(function(receipt){
+      assert.equal(receipt.logs.length,1,'triggers one event');
+      assert.equal(receipt.logs[0].event,'Approval','should be the Approval event');
+      assert.equal(receipt.logs[0].args._owner,accounts[0],'logs the account the token authorized by');
+      assert.equal(receipt.logs[0].args._spender,accounts[1],'logs the account authroized to transfer token');
+      assert.equal(receipt.logs[0].args._value,100,'logs the value approved');
+      return tokenInstance.allowance(accounts[0],accounts[1]);
+    }).then(function(allowance){
+      assert.equal(allowance,100,"stores the allowance for delegated transfer");
+    });
+  });
 })
